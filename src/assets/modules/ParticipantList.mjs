68d86650b1df91ac93b050d16fc6e9f445dcc55c
jascha <jascha@jaeh.at>
2020-04-05 00:20:45 +0200

    Raffle now randomizes

--- src/assets/modules/ParticipantList.mjs
+++ src/assets/modules/ParticipantList.mjs
@@ -1,13 +1,5 @@
 export const View = (tokens = []) => {
-  return ul(tokens
-    .sort((a, b) => Math.random()  - Math.random())
-    .map(token => li(['participant: ', token.tok])))
+  return ul(
+    tokens.map(({ tok, id }) => li({ class: `Participant p-${id}` }, ['participant: ', tok])),
+  )
 }
-
-// export const subscriptions = [
-  // (dispatch, options) => {
-    // const interval = setInterval(() => dispatch(options.action), options.time)
-    // console.log('subscription');
-    // return () => clearInterval(interval)
-  // },
-// ]
