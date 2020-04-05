9737ff15238278a1a835d45480bf2e4889bdc402
jascha <jascha@jaeh.at>
2020-04-05 15:50:23 +0200

    update Raffle and ParticipantList

--- src/assets/modules/ParticipantList.mjs
+++ src/assets/modules/ParticipantList.mjs
@@ -1,39 +1,9 @@
-export const View = (tokens = [], prices = [6, 16]) => {
+export const View = ({tokens = [], wins}, children) => {
   return ul(
     tokens.map(({ tok, id }, i) => [
-      i === 0 && [
-        li([
-          h3(
-            Link({
-              text: 'Status Keycard raffle NFT',
-              to:
-                'https://mintbase.io/pez-dispenser/0x0b23ad11e92fdf87eb5c9b0f24b9860f9c4689ee/SknAvIIUGxlvoU0iJ7XG',
-            }),
-          ),
-          p(
-            'This Mintbase NFT represents a raffle ticket offering the chance to win 1 of 10 Status Keycards.',
-          ),
-        ]),
-        li(h3('Winners:')),
-      ],
-      i === prices[0] && [
-        li([
-          h3(
-            Link({
-              text: 'Shift Crypto Wallet Raffle NFT',
-              to:
-                'https://mintbase.io/pez-dispenser/0x0b23ad11e92fdf87eb5c9b0f24b9860f9c4689ee/OjZ2GaixwXmjOwM6Tgsw',
-            }),
-          ),
+      i === 0 && children,
 
-          p(
-            'This Mintbase NFT represents a raffle ticket offering the chance to win 1 of 6 Shift Crypto Wallets.',
-          ),
-        ]),
-        li(h3('Winners:')),
-      ],
-
-      i === prices[1] && [
+      i === wins - 1 && [
         li([h3('Sorry, all others, better luck next time.'), h4('Other participants:')]),
       ],
 
